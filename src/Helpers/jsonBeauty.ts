f08fbfb69fd4d2219cc29f460acf828ea5f1453b
lufabaz <lufabaz@gmail.com>
2020-08-11 20:01:59 -0300

    Result GET Items Implementation

---
+++ src/Helpers/jsonBeauty.ts
@@ -0,0 +1,56 @@
+const replacer = (_key, value) => (value === null ? undefined : value)
+
+const stringify = obj => {
+  JSON.stringify(obj, replacer)
+}
+
+const syntaxHighlight = (json: string) => {
+  json = json
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+
+  return json.replace(
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)/g,
+    match => {
+      let cls = 'color: darkorange;'
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = 'color: red;'
+        } else {
+          cls = 'color: green;'
+        }
+      } else if (/true|false/.test(match)) {
+        cls = 'color: blue;'
+      } else if (/null/.test(match)) {
+        cls = 'color: magenta;'
+      }
+      return `<span style="${cls}">${match}</span>`
+    }
+  )
+}
+
+const prettyJson = (payload: string | Record<string, any>): string => {
+  let json: Record<string, any>
+  try {
+    json = JSON.parse(payload as string)
+  } catch (e) {
+    json = payload as Record<string, any>
+  }
+
+  if (typeof json === 'object') {
+    Object.entries(json).forEach(([key, value]) => {
+      if (Array.isArray(value)) {
+        value = value.map(element => {
+          prettyJson(element)
+        })
+      } else if (typeof value === 'string' && value.match(/"+:+"/)) {
+        json[key] = JSON.parse(value)
+      }
+    })
+  }
+
+  return JSON.stringify(json, null, 2)
+}
+
+export { stringify, syntaxHighlight, prettyJson }
--- src/components/meli/ResultItems.js
+++ src/components/meli/ResultItems.js
@@ -1,17 +1,41 @@
 import React, {useState} from 'react'
+import * as pretty from '../../Helpers/jsonBeauty.ts'
 
 export default function ResultItems({onGetResult}) {
-    let stringfyResult = JSON.stringify(onGetResult)
-    console.log(onGetResult)
+    let stringPretty = pretty.prettyJson(onGetResult)
+    let highligth = pretty.syntaxHighlight(stringPretty)
+    console.log(highligth)
 
     return (
         <div>
-            {stringfyResult}
- {/*            {onGetResult.length > 0 && onGetResult.foreach((res) => {
-                return (
-                    res
-                )
-            })} */}
+            {<div style={styles.div}>
+                {highligth}
+            </div>}
         </div>
     )
+}
+
+const styles = {
+    div: {
+        padding: '30px',
+        border: '1px solid lightgray',
+        borderRadius: '5px',
+        margin: '15px',
+        width: '970px'
+    },
+    string: {
+        color: 'green' 
+    },
+    number: {
+        color: 'darkorange'
+    },
+    boolean: {
+        color: 'blue'
+    },
+    null: {
+        color: 'magenta'
+    },
+    key: {
+        color: 'red'
+    }
 }
