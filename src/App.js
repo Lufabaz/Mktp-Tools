db8afbf5e01ecdc3417de0582e2ee39dc4e1f53e
LuFaBaz <lufabaz@gmail.com>
2020-08-10 21:32:47 -0300

    Formatter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-/* import Preloader from './components/Preloader'
- */import Meli from './components/Meli'
+import Meli from './components/Meli'
+/* import Preloader from './components/Preloader'*/
 
 export default function App() {
 
