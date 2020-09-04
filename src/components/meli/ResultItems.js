33aeabebe4d546f53796420d4c2b8de84fc8e525
LuFaBaz <lufabaz@gmail.com>
2020-09-03 22:19:35 -0300

    Include module react-json-pretty as helper

--- src/components/meli/ResultItems.js
+++ src/components/meli/ResultItems.js
@@ -1,27 +1,15 @@
 import React from 'react'
 import css from './resultitems.module.css'
+import JSONPretty from '../../Helpers/react-json-pretty'
+import acai from '../../Helpers/react-json-pretty/themes/acai.css'
 
-/* import * as pretty from '../../Helpers/jsonBeauty.ts'
-import parse from 'html-react-parser' */
-import JSONPretty from 'react-json-pretty'
-import acai from 'react-json-pretty/themes/acai.css';
+/* import * as pretty from '../../Helpers/jsonBeauty.ts'*/
 
 export default function ResultItems({onGetResult}) {
-/*     let stringResult = JSON.stringify(onGetResult)
-    let stringPretty = pretty.prettyJson(onGetResult)
-    let htmlParse = parse(stringPretty) */
-
- /*    let htmlParse = parse(onGetResult)
-    let stringPretty = pretty.prettyJson(onGetResult)
-    let highligth = pretty.syntaxHighlight(stringPretty)
-    
-    console.log(typeof onGetResult) */
-/*     console.log(htmlParse)
- */
 
     return (
         <div className={css.content}>
-            <hr style={styles.hr} />
+            <hr style={css.hr} />
             <div className={css.tabcontents}>
                 <section>
                     <span className={css.collapsible}>
@@ -32,29 +20,3 @@ export default function ResultItems({onGetResult}) {
         </div>
     )
 }
-
-const styles = {
-    div: {
-        width: 'auto',
-    },
-    string: {
-        color: 'green' 
-    },
-    number: {
-        color: 'darkorange'
-    },
-    boolean: {
-        color: 'blue'
-    },
-    null: {
-        color: 'magenta'
-    },
-    key: {
-        color: 'red'
-    },
-    hr: {
-        width: 'auto',
-        border: '1px solid lightgray'
-    }
-}
-
