ddbd143469178d40ed355f9cfd10fc9be330e23f
Lufabaz <lufabaz@gmail.com>
2020-10-04 20:28:56 -0300

    Add New Marketplace - Zoom - Notification Orders

--- src/components/Zoom.js
+++ src/components/Zoom.js
@@ -72,7 +72,7 @@ export default function Zoom({onClickResource}) {
       }).then((response) => { 
         console.log(response.headers.get('Content-Type'));
         console.log(response.headers.get('Date'));
-    
+     
         console.log(response.status);
         console.log(response.type);
         console.log(response.url);
